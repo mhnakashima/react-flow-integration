b24aa2d6eed4764e3af805148c2017990238fdd8
michael haruo nakashima <mhnakashima@gmail.com>
2021-08-05 13:59:25 -0300

    creating visualization of json data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ReactFlow, { MiniMap, Controls, Background } from "react-flow-renderer";
 
 function App() {
+  const flow = require("./assets/data.json");
+  let elements = [];
+
+  if (flow) {
+    for (const [key, value] of Object.entries(flow)) {
+      const x = parseInt(value.$position.left.split("px")[0]) || 0;
+      const y = parseInt(value.$position.top.split("px")[0]) || 0;
+      elements.push({
+        id: key,
+        data: {
+          label: <div className="bloco">{value.$title}</div>,
+        },
+        position: { x, y },
+      });
+
+      if (value.$conditionOutputs.length) {
+        const edges = value.$conditionOutputs;
+        edges.forEach(edge => {
+          elements.push({
+            id: `el_${key}_${edge.stateId}`,
+            source: key,
+            target: edge.stateId,
+            animated: true
+          })
+        })
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          width: window.screen.availWidth,
+          height: window.screen.availHeight - 105,
+        }}
+      >
+        <ReactFlow className="builder-container" elements={elements}>
+          <MiniMap
+            nodeColor={(node) => {
+              switch (node.type) {
+                case "input":
+                  return "red";
+                case "default":
+                  return "#00ff00";
+                case "output":
+                  return "rgb(0,0,255)";
+                default:
+                  return "#eee";
+              }
+            }}
+            nodeStrokeWidth={3}
+          />
+          <Background variant="dots" gap={12} size={0.5} />
+          <Controls />
+        </ReactFlow>
+      </div>
     </div>
   );
 }
